0ed13f8bcd1fd1623ff2f6792856d63805925709
王思源 <wangsiyuan@Dont-Tension.local>
2014-11-05 09:24:43 +0800

    Initial Commit

---
+++ imme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imme
+//
+//  Created by 王思源 on 14/11/5.
+//  Copyright (c) 2014年 immeWithDouBi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ imme/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  imme
+//
+//  Created by 王思源 on 14/11/5.
+//  Copyright (c) 2014年 immeWithDouBi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
